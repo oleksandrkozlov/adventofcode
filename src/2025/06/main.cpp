e65bef06b25a7f3745526d90c708c1797abafe33
Oleksandr Kozlov <aleks.a.kozlov@gmail.com>
2025-12-12 20:32:20 +0100

    Refactor puzzle 2025_06

--- src/2025/06/main.cpp
+++ src/2025/06/main.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-#include <range/v3/all.hpp>
 
 namespace {
 
@@ -17,67 +16,92 @@ constexpr auto input = std::invoke([] {
     return std::string_view{result, sizeof(result)};
 });
 
-template<class Rngs>
-struct interleave_view : public ranges::view_facade<interleave_view<Rngs>> {
-    struct cursor;
+template<rng::input_range Rngs>
+    requires rng::input_range<rng::range_reference_t<Rngs>>
+class interleave_view : public rng::view_interface<interleave_view<Rngs>> {
+public:
+    struct iterator;
 
     interleave_view() = default;
 
     explicit interleave_view(Rngs rs)
-        : rngs(std::move(rs) | rng::to<std::vector>())
+        : rngs{std::move(rs) | rng::to<std::vector>()}
     {
     }
 
-    auto begin_cursor() -> cursor
+    [[nodiscard]] auto begin() -> iterator
     {
         return {0, &rngs, rv::transform(rngs, rng::begin) | rng::to<std::vector>()};
     }
 
-    std::vector<ranges::range_value_t<Rngs>> rngs;
+    [[nodiscard]] auto end() const -> std::default_sentinel_t
+    {
+        return std::default_sentinel;
+    }
+
+    std::vector<rng::range_value_t<Rngs>> rngs;
 };
 
-template<class Rngs>
-struct interleave_view<Rngs>::cursor {
+template<rng::input_range Rngs>
+    requires rng::input_range<rng::range_reference_t<Rngs>>
+struct interleave_view<Rngs>::iterator {
+    using value_type = rng::range_value_t<rng::range_reference_t<Rngs>>;
+    using difference_type = std::ptrdiff_t;
 
-    auto read() const -> decltype(auto)
+    [[nodiscard]] auto operator*() const -> value_type
     {
         return *its[n];
     }
 
-    auto next() -> void
+    auto operator++() -> iterator&
     {
         if (0 == ((++n) %= its.size())) {
             rng::for_each(its, [](auto& it) { ++it; });
         }
+        return *this;
     }
 
-    auto equal(ranges::default_sentinel_t) const -> bool
+    auto operator++(int) -> void
     {
-        if (n != 0) { return false; }
-        auto ends = *rngs | rv::transform(rng::end);
-        return its.end() != std::mismatch(its.begin(), its.end(), ends.begin(), std::not_equal_to<>{}).first;
+        ++*this;
     }
 
-    auto equal(cursor const& that) const -> bool
+    [[nodiscard]] friend auto operator==(const iterator& it, std::default_sentinel_t) -> bool
+    {
+        if (it.n != 0) { return false; }
+        auto ends = *it.rngs | rv::transform(rng::end);
+        return it.its.end() != std::mismatch(it.its.begin(), it.its.end(), ends.begin(), std::not_equal_to<>{}).first;
+    }
+
+    [[nodiscard]] auto operator==(const iterator& that) const -> bool
     {
         return n == that.n && its == that.its;
     }
 
     std::size_t n{};
-    std::vector<ranges::range_value_t<Rngs>>* rngs;
-    std::vector<ranges::iterator_t<ranges::range_value_t<Rngs>>> its;
+    std::vector<rng::range_value_t<Rngs>>* rngs;
+    std::vector<rng::iterator_t<rng::range_value_t<Rngs>>> its;
 };
 
-auto interleave()
-{
-    return ranges::make_view_closure(
-        []<typename Rngs>(Rngs&& rngs) { return interleave_view{std::forward<Rngs>(rngs)}; });
-}
+struct interleave_view_t : rng::range_adaptor_closure<interleave_view_t> {
 
-auto transpose()
-{
-    return ranges::make_view_closure([](auto&& rngs) { return rngs | interleave() | rv::chunk(rng::distance(rngs)); });
-}
+    auto operator()(auto&& rngs) const
+    {
+        return interleave_view{std::move(rngs)};
+    }
+};
+
+constexpr auto interleave = interleave_view_t{};
+
+struct transpose_view_t : rng::range_adaptor_closure<transpose_view_t> {
+
+    auto operator()(auto&& rngs) const
+    {
+        return rngs | interleave | rv::chunk(rng::distance(rngs));
+    }
+};
+
+constexpr auto transpose = transpose_view_t{};
 
 constexpr auto to_sv = [](auto rng) { return std::string_view{rng}; };
 constexpr auto to_num = [](auto sv) {
@@ -91,11 +115,11 @@ auto part_one() -> std::size_t
     auto lines = input | rv::split('\n') | rv::filter(std::not_fn(rng::empty)) | rv::transform([](auto&& line) {
         return line | rv::split(' ') | rv::filter(std::not_fn(rng::empty)) | rv::transform(to_sv);
     });
-    return rng::fold_left(lines | transpose() | rv::transform([](auto&& line) {
+    return rng::fold_left(lines | transpose | rv::transform([](auto&& line) {
         auto not_sign = [](auto&& rng) { return rng != "+" and rng != "*"; };
-        auto nums = line | rv::take_while(not_sign) | rv::transform(to_num);
-        return (line | rv::drop_while(not_sign)).front() == "*" ? rng::fold_left(nums, 1uz, std::multiplies{})
-                                                                : rng::fold_left(nums, 0uz, std::plus{});
+        auto nums = line | rv::take_while(not_sign) | rv::transform(to_num) | rng::to<std::vector>();
+        return *(line | rv::drop_while(not_sign)).begin() == "*" ? rng::fold_left(nums, 1uz, std::multiplies{})
+                                                                 : rng::fold_left(nums, 0uz, std::plus{});
     }), 0uz, std::plus{}); // clang-format on
 }
 
@@ -105,9 +129,10 @@ auto part_two() -> std::size_t
     auto is_blank = [&](const std::string_view sv) { return rng::all_of(sv, is_space); };
     auto to_str = [](auto&& rng) { return rng | rng::to<std::string>(); };
     auto lines = input | rv::split('\n') | rv::filter(std::not_fn(rng::empty));
-    auto columns = lines | transpose();
-    auto chunks = columns // clang-format off
-        | ranges::views::chunk_by([&](auto&& rhs, auto&& lhs) { return !is_blank(to_str(rhs)) and !is_blank(to_str(lhs)); })
+    auto chunks = lines // clang-format off
+        | transpose
+        | rng::to<std::vector<std::string>>()
+        | rv::chunk_by([&](auto&& rhs, auto&& lhs) { return !is_blank(to_str(rhs)) and !is_blank(to_str(lhs)); })
         | rv::filter([&](auto&& chunk) { return !is_blank(to_str(chunk.front())); }) // clang-format on
         | rng::to<std::vector<std::deque<std::string>>>();
     for (auto&& chunk : chunks) {
